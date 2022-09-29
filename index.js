ede80d836251d28f2ba6d5824d6aad294b0baaa1
KaiqueYudji <yudjikaique@gmail.com>
2022-09-29 14:12:00 -0300

    almostReady

--- index.js
+++ index.js
@@ -1,80 +1,34 @@
-let tb_eSim_roof = [
-    {
-      id_eSim_roof:1,
-      id_country:"Estados Unidos",
-      id_plan:"MegaPack",
-      priority:"****"
-    },
-
-    {
-        id_eSim_roof:1,
-        id_country:"Estados Unidos",
-        id_plan:"MAX20GB",
-        priority:"**"
-      },
-
-      {
-        id_eSim_roof:1,
-        id_country:"Estados Unidos",
-        id_plan:"ESPECIAL",
-        priority:"**"
-      },
-
-      {
-        id_eSim_roof:1,
-        id_country:"Brasil,Argentina,Canadá,Chile,Costa Rica,Colômbia,El Salvador,Equador,Estados Unidos,Guadalupe,Guatemala,Honduras,México,Nicarágua,Panamá,Peru,Paraguai,Porto Rico,República Dominicana,Uruguai,Alemanha,Albania,Áustria,Belarus,Bélgica,Bulgária,Chipre,Croácia,Dinamarca,Eslováquia,Eslovênia,Espanha,Estônia,Finlândia,França,Grécia,Gibraltar,Holanda,Hungria,Islândia,Ilhas Faroe,Irlanda,Itália,Letônia,Liechtenstein,Lituânia,Luxemburgo,Malta,Montenegro,Noruega,Portugal,Polônia,Inglaterra,Escócia,País de Gales,República Checa,Romênia,Suíça,Suécia,Austrália,Azerbaidjão,Cazaquistão,Coreia do Sul,China,Emirados Árabes,Georgia,Hong Kong,Indonésia,Israel,Malásia,Qatar,Rússia,Singapura,Tailândia,Taiwan,Turquia,Ucrânia,Uzbequistão,Vietnam,Egito", 
-        id_plan:"TOP DESTINO",
-        priority:"**"
-      },
-
-      {
-        id_eSim_roof:1,
-        id_country:"Estados Unidos,Canadá",
-        id_plan:"Global",
-        priority:"*"
-      },
-      {
-        id_eSim_roof:1,
-        id_country:"Canadá",
-        id_plan:"TOP DESTINO",
-        priority:"****"
-      },
-      {
-        id_eSim_roof:1,
-        id_country:"Canadá",
-        id_plan:"ESPECIAL",
-        priority:"**"
-      },
-];
+const tb_eSim_roof = require("./datas.json")
 
 
 function FilteringPlans(destinyCountry){
-   let qtdCountries = destinyCountry.length;
-   let searchPlan = [];
-      
-        if(qtdCountries === 2)
-        searchPlan.push( ...(tb_eSim_roof.filter(plan => plan.id_country.indexOf(destinyCountry[0]) !== -1    &&    plan.id_country.indexOf(destinyCountry[1]) !== -1  ) ) );
-
-        else if(qtdCountries === 3)
-        searchPlan.push( ...(tb_eSim_roof.filter(plan => plan.id_country.indexOf(destinyCountry[0]) !== -1    &&    plan.id_country.indexOf(destinyCountry[1]) !== -1    &&    plan.id_country.indexOf(destinyCountry[2]) !== -1  ) ) );
-
-        else if(qtdCountries === 4)
-        searchPlan.push( ...(tb_eSim_roof.filter(plan => plan.id_country.indexOf(destinyCountry[0]) !== -1    &&    plan.id_country.indexOf(destinyCountry[1]) !== -1    &&    plan.id_country.indexOf(destinyCountry[2]) !== -1    &&    plan.id_country.indexOf(destinyCountry[3]) !== -1  ) ) );
+   let planObject = new Object();
+   let count = destinyCountry.length;
    
-        else if(qtdCountries === 5)
-        searchPlan.push( ...(tb_eSim_roof.filter(plan => plan.id_country.indexOf(destinyCountry[0]) !== -1    &&    plan.id_country.indexOf(destinyCountry[1]) !== -1    &&    plan.id_country.indexOf(destinyCountry[2]) !== -1    &&    plan.id_country.indexOf(destinyCountry[3]) !== -1    &&    plan.id_country.indexOf(destinyCountry[4]) !== -1 ) ) );
-   
-        else if(qtdCountries === 6)
-        searchPlan.push( ...(tb_eSim_roof.filter(plan => plan.id_country.indexOf(destinyCountry[0]) !== -1    &&    plan.id_country.indexOf(destinyCountry[1]) !== -1    &&    plan.id_country.indexOf(destinyCountry[2]) !== -1    &&    plan.id_country.indexOf(destinyCountry[3]) !== -1     &&    plan.id_country.indexOf(destinyCountry[4]) !== -1    &&    plan.id_country.indexOf(destinyCountry[5]) !== -1  ) ) );
-        
-        else if(qtdCountries === 7)
-        searchPlan.push( ...(tb_eSim_roof.filter(plan => plan.id_country.indexOf(destinyCountry[0]) !== -1    &&    plan.id_country.indexOf(destinyCountry[1]) !== -1    &&    plan.id_country.indexOf(destinyCountry[2]) !== -1    &&    plan.id_country.indexOf(destinyCountry[3]) !== -1     &&    plan.id_country.indexOf(destinyCountry[4]) !== -1    &&    plan.id_country.indexOf(destinyCountry[5]) !== -1    &&    plan.id_country.indexOf(destinyCountry[6]) !== -1  ) ) );
+   let i2 = 0;
+    for(let i = 0; i < count; i++){
+          let plan = tb_eSim_roof.filter(plan => plan.id_country.indexOf(destinyCountry[i].toLowerCase()) !== -1) ;
 
-        else if(qtdCountries === 8)
-        searchPlan.push( ...(tb_eSim_roof.filter(plan => plan.id_country.indexOf(destinyCountry[0]) !== -1    &&    plan.id_country.indexOf(destinyCountry[1]) !== -1    &&    plan.id_country.indexOf(destinyCountry[2]) !== -1    &&    plan.id_country.indexOf(destinyCountry[3]) !== -1     &&    plan.id_country.indexOf(destinyCountry[4]) !== -1    &&    plan.id_country.indexOf(destinyCountry[5]) !== -1    &&    plan.id_country.indexOf(destinyCountry[6]) !== -1    &&    plan.id_country.indexOf(destinyCountry[7]) !== -1 ) ) );
-   
-    
-    return searchPlan;
+          plan.forEach(p => {
+              let country = destinyCountry[i].toString().toLowerCase();
+              i2++;
+              
+              if(i2 === 1){
+                planObject[p.id_plan] = {[country]:p.priority};
+              }else{
+                  if(!planObject[p.id_plan]){
+                    planObject[p.id_plan] = {[country]:p.priority};
+                  }else{
+                      planObject[p.id_plan] = [planObject[p.id_plan]].concat(  {[country]:p.priority});  
+                  }
+              } 
+                          
+            }); 
+      }
+ 
+      //I don´t need return aa array, but i want to use the operator spread so I'll return an array
+    let final = [planObject]
+    return final;
 }
 
 
@@ -85,7 +39,6 @@ function FilteringPlans(destinyCountry){
 const betterPlans = (plans,destinyCountry) => {
   let betters = [];
   let priorities = []
-  
 
     if(destinyCountry.length === 1){
         plans.forEach(plan => {
@@ -122,23 +75,22 @@ const betterPlans = (plans,destinyCountry) => {
 
 
 function guidedSelling(destinyCountry){
-  let searchPlan;
-
+  let searchPlan = [];
 
       if(destinyCountry.length === 1){
-        searchPlan = FilteringPlans(destinyCountry);
+        searchPlan.push( ...(tb_eSim_roof.filter(plan => plan.id_country.indexOf(destinyCountry[0].toLowerCase()) !== -1 )) )
       }  
       else{
-        searchPlan = FilteringPlans(destinyCountry);
+        searchPlan.push( ...FilteringPlans(destinyCountry) );
       }                          
 
-   console.log(searchPlan)
-  let result = betterPlans(searchPlan,destinyCountry);
-  console.log(result);  
+  console.log(searchPlan);    
+  //let result = betterPlans(searchPlan,destinyCountry);
+  
 
 }
 
-guidedSelling(["Estados Unidos","Canadá"])
+guidedSelling(["Estados Unidos","Canada"])
 
 
 
